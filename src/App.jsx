a4cceab5e4daa7f7e221f3ead9d9fdef671f194d
Fedro Togatorop <fedrotoga@gmail.com>
2025-08-06 14:40:26 +0700

    add index from pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
 import Article from "./components/Article";
+import Home from "./pages/index";
 // import "./components/Article";
 function App() {
   return (
     <div className="App">
-      {/* <Article name="Fedro" /> */}
-      <Article name="Fedro" titles={["VueJS", "NodeJS", "TypeSkript"]} />
-      <br />
-      {/* <Article name="Togatorop" titles={["ReactJS", "NextJS"]} /> */}
+      <Home />
+      {/* <Article name="Fedro" titles={["react", "nextJS", "NodeJS"]} />
+      <Article name="Togatorop" titles={["Javascript", "PHP"]}></Article> */}
     </div>
   );
 }
