2f18cfc3ca2729a977305c6ef8862b1c91c79521
Fedro Togatorop <fedrotoga@gmail.com>
2025-08-06 17:16:54 +0700

    menambahkan fitur search

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import posts from "../posts.json";
 import Article from "../components/Article";
 
 function Homepage() {
+  // let search = "";
+  const [search, setSearch] = useState("");
+  const changeSearch = (event) => {
+    setSearch(event.target.value);
+  };
   return (
     <>
       <h1>Simple Blog</h1>
-      {posts.map((blog) => (
-        <Article title={blog.title} tags={blog.tags} date={blog.date} />
+      <div>
+        Cari Artikel : <input onChange={changeSearch}></input>
+      </div>
+      <small>ditemukan 0 data dengan pencarian kata {search}</small>
+      {posts.map(({ title, tags, date }) => (
+        // <Article title={title} tags={tags} date={date} />
+        <Article {...{ title, tags, date }} />
       ))}
     </>
   );
